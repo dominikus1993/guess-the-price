80782cdf4560d2a51d76c1798fde9c50bd0d6990
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-09-12 12:20:51 +0200

    .

--- src/GuessThePrice.WebApp/Program.cs
+++ src/GuessThePrice.WebApp/Program.cs
@@ -1,40 +1,23 @@
-using GuessThePrice.Core.Grains;
-using GuessThePrice.Core.Services;
-
-using Orleans;
-using Orleans.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Host.UseOrleans(b =>
-{
-    b.AddMemoryGrainStorage("games");
-    b.UseDashboard(options =>
-    {
-        options.Port = 8888;
-    }).ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(GameGrain).Assembly));
-});
-builder.Services.AddTransient<IProductsDataProvider, ProductsDataProvider>();
-
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseStaticFiles();
-app.UseRouting();
+app.UseAuthorization();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");;
+app.MapControllers();
 
 app.Run();
