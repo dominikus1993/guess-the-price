92e16f3e7ab2b9a40dc8b090e5bc13e63cc512d2
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-09-14 12:33:33 +0200

    add tests

--- tests/GuessThePrice.Tests/Core/Grains/GameGrainTests.cs
+++ tests/GuessThePrice.Tests/Core/Grains/GameGrainTests.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 
 using GuessThePrice.Core.Grains;
+using GuessThePrice.Core.Model;
 using GuessThePrice.Tests.Core.Fixtures;
 
 namespace GuessThePrice.Tests.Core.Grains;
@@ -48,5 +49,60 @@ public class GameGrainTests : IClassFixture<OrleansGrainFixture>
         var game = await grain.GetGame();
 
         game.IsInitialized.Should().BeTrue();
+    }
+    
+            
+    [Fact]
+    public async Task TestAddOneResponseWhenGameIsNotInitialized()
+    {
+        var id = Guid.NewGuid();
+        var grain = _fixture.Cluster.GrainFactory.GetGrain<IGameGrain>(id);
+        await grain.AddResponse(new Response(1, new PromotionalPriceResponse(2)));
+        var subject = await grain.GetGame();
+        subject.IsInitialized.Should().BeFalse();
+        subject.Responses.Should().BeEmpty();
+        subject.Products.Should().BeEmpty();
+    }
+        
+    [Fact]
+    public async Task TestAddOneResponseWhenGameIsInitialized()
+    {
+        var id = Guid.NewGuid();
+        var grain = _fixture.Cluster.GrainFactory.GetGrain<IGameGrain>(id);
+        var game = await grain.StartGame();
+        await grain.AddResponse(new Response(game.Products.First().Id, new PromotionalPriceResponse(2)));
+        var subject = await grain.GetGame();
+        subject.IsInitialized.Should().BeTrue();
+        subject.Responses.Should().NotBeEmpty();
+        subject.Responses.Should().HaveCount(1);
+        subject.Products.Should().NotBeEmpty();
+    }
+    
+    [Fact]
+    public async Task TestAddMoreResponsesThanProductsResponseWhenGameIsInitialized()
+    {
+        var id = Guid.NewGuid();
+        var grain = _fixture.Cluster.GrainFactory.GetGrain<IGameGrain>(id);
+        var game = await grain.StartGame();
+        await grain.AddResponse(new Response(game.Products.First().Id, new PromotionalPriceResponse(2)));
+        await grain.AddResponse(new Response(game.Products.First().Id, new PromotionalPriceResponse(2)));
+        var subject = await grain.GetGame();
+        subject.IsInitialized.Should().BeTrue();
+        subject.Responses.Should().NotBeEmpty();
+        subject.Responses.Should().HaveCount(1);
+        subject.Products.Should().NotBeEmpty();
+    }
+    
+    [Fact]
+    public async Task TestAddResponseForGameThatNotExists()
+    {
+        var id = Guid.NewGuid();
+        var grain = _fixture.Cluster.GrainFactory.GetGrain<IGameGrain>(id);
+        var game = await grain.StartGame();
+        await grain.AddResponse(new Response(50000, new PromotionalPriceResponse(2)));
+        var subject = await grain.GetGame();
+        subject.IsInitialized.Should().BeTrue();
+        subject.Responses.Should().BeEmpty();
+        subject.Products.Should().NotBeEmpty();
     }
 }
